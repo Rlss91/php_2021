1746fafa781821e32491d5c9715be73799d9d735
rlss91 <rlss91@gmail.com>
2021-10-17 21:02:20 +0300

    another way

---
+++ another way.php
@@ -0,0 +1,35 @@
+<?php
+    $us = "kenny";
+    $ps = "123456";
+    $title = "Another way";
+    $lang = "en";
+?>
+
+<!DOCTYPE html>
+<?php if($lang =='en'): ?>
+<html lang="en">
+<?php else: ?>
+<html lang="he" dir="rtl">
+<?php endif; ?>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?=$title?></title>
+    <style>
+      html,
+      body {
+        font-size: 2rem;
+      }
+    </style>
+</head>
+<body>
+    <ul>
+        <?php for($i=0;$i<5;$i++): ?>
+            <?php if($i%2): ?>
+                <li><?=$i+1?></li>
+            <?php endif; ?>
+        <?php endfor; ?>
+    </ul>
+</body>
+</html>
