f1128d3a6055c6f8120769bfe3f703c76971d691
rlss91 <rlss91@gmail.com>
2021-10-20 17:50:12 +0300

    scope

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Functions</title>
+    
+</head>
+<body>
+    <div>
+    <?php 
+    
+
+        function helloWord(){
+            $i = 0;
+            echo "hello world from function!<br />";
+            
+        }
+        $i = 1;
+
+        helloWord();
+
+        {
+            $i=5;
+            echo $i;
+        }
+        echo "<br />";
+        echo $i;
+    ?>
+    </div>
+</body>
+</html>
