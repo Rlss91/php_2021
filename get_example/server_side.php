643650b93bb5e9c0b0277d5915b079f8086bd2b1
rlss91 <rlss91@gmail.com>
2021-10-27 18:50:20 +0300

    get example

---
+++ get_example/server_side.php
@@ -0,0 +1,26 @@
+<?php 
+
+    if(isset($_GET['email'])){
+        echo $_GET['email'];
+        echo "<br />";
+    }
+    if(isset($_GET['password'])){
+        echo $_GET['password'];
+        echo "<br />";
+    }
+    
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    your data here
+</body>
+</html>
